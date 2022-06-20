6648bc3b46c0971d08ecd6b9422c6b432e38d57e
JOY <joy.kemunto@student.moringaschool.com>
2022-06-20 19:13:52 +0300

    commit tree b4320001e53075500e06e8e0454d6f72262c5ec8

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function isPalindrome(word) {
   // Write your algorithm here
 }
 
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
+
 /* 
   Add your pseudocode here
 */
